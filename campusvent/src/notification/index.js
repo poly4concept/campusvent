1a3b20f5e49ca162657cc6dccaee267f1752eb43
Poly4 <olajuwonmubarak355@gmail.com>
2022-01-15 15:04:33 +0100

    implement the notification section

--- campusvent/src/notification/index.js
+++ campusvent/src/notification/index.js
@@ -1,10 +1,50 @@
 import React from 'react'
 import './notification.css'
+import Organizer1 from '../assets/Member_10.png'
+import Organizer2 from '../assets/Member_8.png'
+import Button from '../button'
 
 const Notification = () => {
     return (
         <div className='notification'>
-            Notification
+            <div className='notification-container'>
+                <div className='popular-box'>
+                    <h5>Popular Events</h5>
+                    <div className='popular-event'>
+                        <h6>1. NextJs, The modern React framework</h6>
+                        <p>286 people attending</p>
+                    </div>
+                    <div className='popular-event'>
+                        <h6>2. React Native, the advanced part</h6>
+                        <p>120 people attending</p>
+                    </div>
+                    <div className='popular-event'>
+                        <h6>1. NextJs, The modern React framework</h6>
+                        <p>286 people attending</p>
+                    </div>
+                    <a href=''>See more</a>
+                </div>
+
+                <div className='organizer-box'>
+                    <h5>Organizers to follow</h5>
+                    <div className='organizer-div'>
+                        <img src={Organizer1} alt="organizer1" className="organizer-img" width="45" height="45" />
+                        <div  className='organizer-info'>
+                            <p className='organizer-name'>Kim Anderson</p>
+                            <p className='organizer-field'>Product Management</p>
+                            <button>Follow</button>
+                        </div>
+                    </div>
+                    <div className='organizer-div'>
+                        <img src={Organizer2} alt="organizer2" className="organizer-img" width="45" height="45" />
+                        <div className='organizer-info'>
+                            <p className='organizer-name'>Dev Phenom</p>
+                            <p className='organizer-field'>Frontend Development</p>
+                            <button>Follow</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
